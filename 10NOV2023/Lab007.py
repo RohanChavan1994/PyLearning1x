7038ff426949e2ec8e8e4f96acb3b7be88c3bd4c
RohanChavan1994 <rohanforjobs@gmail.com>
2023-11-13 18:49:49 +0530

    Modified the file

--- 10NOV2023/Lab007.py
+++ 10NOV2023/Lab007.py
@@ -1,7 +1,11 @@
-a = 1
-b = 0
+a = int(input("Enter number 1:"))
+b = int(input("Enter number 2:"))
 
 try:
-    print(a/b)
+    c = a / b
 except Exception as error:
     print("Error occurred:", error)
+else:
+    print(c)
+finally:
+    print("This finally block")
